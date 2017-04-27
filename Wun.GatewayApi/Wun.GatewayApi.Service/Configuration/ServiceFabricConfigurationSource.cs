c066406abeaf2cf88a3b1cfb114677e953d9cc4a
HUMANACTION\Krzysztof <jksikora90@gmail.com>
2017-04-27 11:51:07 +0100

    Changed Gateway API to use the redis connection string from the service fabric settings not from default aspnet core appsettings.json file.

---
+++ Wun.GatewayApi/Wun.GatewayApi.Service/Configuration/ServiceFabricConfigurationSource.cs
@@ -0,0 +1,20 @@
+﻿using System.Fabric;
+using Microsoft.Extensions.Configuration;
+
+namespace Wun.GatewayApi.Service.Configuration
+{
+    public class ServiceFabricConfigurationSource : IConfigurationSource
+    {
+        private readonly ServiceContext _serviceContext;
+
+        public ServiceFabricConfigurationSource(ServiceContext serviceContext)
+        {
+            _serviceContext = serviceContext;
+        }
+
+        public IConfigurationProvider Build(IConfigurationBuilder builder)
+        {
+            return new ServiceFabricConfigurationProvider(_serviceContext);
+        }
+    }
+}
