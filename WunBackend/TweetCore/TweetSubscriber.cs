16e47a5950aa0b75f76baf7e3384359146023e77
jamesmus <james@musson.me>
2017-04-27 14:24:52 +0100

    Added config for delayed tweets

--- WunBackend/TweetCore/TweetSubscriber.cs
+++ WunBackend/TweetCore/TweetSubscriber.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading.Tasks;
 using StackExchange.Redis;
 using Wun.Backend.TweetModel;
 
@@ -7,13 +6,11 @@ namespace Wun.Backend.TweetCore
 {
     public class TweetSubscriber : IDisposable
     {
-        private readonly Action<Tweet> _callback;
         private readonly ConnectionMultiplexer _redisConnection;
         private readonly ISubscriber _redisSubscriber;
 
         public TweetSubscriber(string connectionString, string channelName, Action<Tweet> callback)
         {
-            _callback = callback;
             _redisConnection = ConnectionMultiplexer.Connect(connectionString);
             _redisSubscriber = _redisConnection.GetSubscriber();
             _redisSubscriber.Subscribe(channelName, (channel, value) => callback(Tweet.Create(value.ToString())));
